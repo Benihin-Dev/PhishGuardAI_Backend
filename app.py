2803c10d26109cd3f9123c56368a60130d253149
Benihin .M <mahendrabenihin@gmail.com>
2025-04-24 22:32:25 +0530

    api intigrationt

--- app.py
+++ app.py
@@ -479,7 +479,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://phish-guard-ai.vercel.app/"],  # Add your frontend URL here
+    allow_origins=["http://localhost:3000", "https://phish-guard-ai-c4vu.vercel.app/"],  # Add your frontend URL here
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
